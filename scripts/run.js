804919fe06da22c174469c52add38ec133469eaf
waynekwest88@gmail.com <waynekwest88@gmail.com>
2021-10-25 17:49:11 -0700

    update run script to include arguments for contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,22 @@
 const main = async () => {
-  const gameContractFactory = await hre.ethers.getContractFactory("NFTGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContractFactory = await hre.ethers.getContractFactory('NFTGame');
+  const characterNames = ['Knight', 'Cowboy', 'Viking'];
+  const characterImageURIs = [
+    'https://cdn.pixabay.com/photo/2017/01/31/16/53/book-2025499_1280.png',
+    'https://cdn.pixabay.com/photo/2017/01/31/23/55/cartoon-2028328_1280.png',
+    'https://cdn.pixabay.com/photo/2019/03/05/19/56/axe-4036928_1280.png',
+  ];
+  const characterHp = [100, 200, 300];
+  const characterAttackDmg = [100, 50, 25];
+  const gameContract = await gameContractFactory.deploy(
+    characterNames,
+    characterImageURIs,
+    characterHp,
+    characterAttackDmg
+  );
   await gameContract.deployed();
   console.log('Contract deployed to:', gameContract.address);
-}
+};
 
 const runMain = async () => {
   try {
