cd86971fb3dfb028d2a3a55131e3acb0421e2252
waynekwest88@gmail.com <waynekwest88@gmail.com>
2021-10-26 19:48:51 -0700

    updated boilerplate code in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,18 @@
 const main = async () => {
-  const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+  const gameContractFactory = await hre.ethers.getContractFactory('NFTGame');
+  const characterNames = ['Knight', 'Cowboy', 'Viking'];
+  const characterImageURIs = [
+    'https://cdn.pixabay.com/photo/2017/01/31/16/53/book-2025499_1280.png',
+    'https://cdn.pixabay.com/photo/2017/01/31/23/55/cartoon-2028328_1280.png',
+    'https://cdn.pixabay.com/photo/2019/03/05/19/56/axe-4036928_1280.png',
+  ];
+  const characterHp = [100, 200, 300];
+  const characterAttackDmg = [100, 50, 25];
   const gameContract = await gameContractFactory.deploy(
-    ['Leo', 'Aang', 'Pikachu'],
-    [
-      'https://i.imgur.com/pKd5Sdk.png',
-      'https://i.imgur.com/xVu4vFL.png',
-      'https://i.imgur.com/u7T87A6.png',
-    ],
-    [100, 200, 300],
-    [100, 50, 25]
+    characterNames,
+    characterImageURIs,
+    characterHp,
+    characterAttackDmg
   );
   await gameContract.deployed();
   console.log('Contract deployed to:', gameContract.address);
